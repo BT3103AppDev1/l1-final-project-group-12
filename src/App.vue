e8f554e995b3c694472d9f65d9dfb18d72096820
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-02 23:19:14 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
