95014929b6570920be78f3ab2d590facdf82bfef
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-03 09:53:29 +0800

    fix spelling in documentation

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,7 +7,7 @@ import { doc, setDoc } from "@firebase/firestore";
 import { auth, db } from "./firebase-config";
 
 /**
- * Trys to sign up a new user using the provided details
+ * Tries to sign up a new user using the provided details
  * @param {string} name new user's name
  * @param {string} phoneNumber new user's phoneNumber
  * @param {string} telegramHandle new user's telegramHandle
@@ -28,7 +28,7 @@ async function signUp(name, phoneNumber, telegramHandle, email, password) {
   }
 }
 /**
- * Trys to sign in an existing user using the provided details
+ * Tries to sign in an existing user using the provided details
  * @param {string} email new user's email
  * @param {string} password new user's password
  */
@@ -41,7 +41,7 @@ async function signIn(email, password) {
 }
 
 /**
- * Trys to sign out the current user
+ * Tries to sign out the current user
  */
 async function logOut() {
   try {
