c979d1346940b98080294fe487cb2ed1f1b524c2
alfredyy <alfredyiow@gmail.com>
2023-03-10 16:01:58 +0800

    listing type selection modal

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LookForStudent from '@/views/LookForStudent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'LookForStudent',
+      component: LookForStudent
+    }
+  ]
 })
 
 export default router
