573737228e3949eb25dfbc9407347b22e38f9cdf
jingyulim1 <jingyulim1@gmail.com>
2023-03-11 18:39:03 +0800

    include views

---
+++ src/views/DisplayStudentListings.vue
@@ -0,0 +1,14 @@
+<template>
+    <StudentListing/>
+</template>
+
+<script>
+import StudentListing from '@/components/StudentListing.vue'
+
+export default {
+    name : 'DisplayStudentListing',
+    components : {
+        StudentListing
+    }
+}
+</script>
