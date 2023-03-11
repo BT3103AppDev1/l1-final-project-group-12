30fb276eff9623201e565c105a787305dfc288f9
jingyulim1 <jingyulim1@gmail.com>
2023-03-11 18:44:17 +0800

    missed out files on previous commit to include views

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
-<template>
-<StudentListing/>
-</template>
-
 <script>
 import { RouterView } from "vue-router";
-import  StudentListing  from "./components/StudentListing.vue";
-
-export default {
-  name: 'App',
-  components: {
-    StudentListing
-  },
-  data(){
-  }
-
-}
 </script>
+
+<template>
+
+
+  <router-view/>
+
+</template>
 
 <style scoped>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DisplayStudentListings from '@/views/DisplayStudentListings.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path : '/',
+      name : 'DisplayStudentListings',
+      component : DisplayStudentListings
+    }
+  ]
 })
 
 export default router
