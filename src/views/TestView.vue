8690692fa7ab0827a48a1dfb8de56e44d2e42ef8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-24 16:26:30 +0800

    flesh out example for modal in TestView

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -8,7 +8,19 @@ const showModal = ref(false);
 <template>
   <p>This is test, truly</p>
   <button @click="showModal = true">show modal</button>
-  <ModalComponent v-show="showModal" @close-modal="showModal = false" />
+  <ModalComponent v-show="showModal" @close-modal="showModal = false">
+    <div id="content">
+      <h1>Example title</h1>
+      <p>example paragraph here</p>
+      <button>example button</button>
+    </div>
+  </ModalComponent>
 </template>
 
-<style scoped></style>
+<style scoped>
+#content {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
