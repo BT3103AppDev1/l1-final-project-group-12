9202bd8b7efe43d31390fdab9b60ef73927de99a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-24 15:25:22 +0800

    added test button for modal

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,3 +1,14 @@
+<script setup>
+import ModalComponent from "@/components/ModalComponent.vue";
+import { ref } from "vue";
+
+const showModal = ref(false);
+</script>
+
 <template>
   <p>This is test, truly</p>
+  <button @click="showModal = true">show modal</button>
+  <ModalComponent v-show="showModal" @close-modal="showModal = false" />
 </template>
+
+<style scoped></style>
