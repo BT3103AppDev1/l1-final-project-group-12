abd876bb0dee1eb61d530192d38e492404fee3f0
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-24 15:25:03 +0800

    abstracted modal component from user-story-3

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-overlay">
+      <div class="modal">
+        <div class="close" @click="$emit('close-modal')">
+          <img class="close-img" src="src\assets\close-icon.png" alt="" />
+        </div>
+
+        <slot>Hello from modal!</slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000da;
+}
+
+.modal {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  min-width: 25%;
+  min-height: 25%;
+  background-color: white;
+  border-radius: 1rem;
+}
+
+.close {
+  width: fit-content;
+  align-self: flex-end;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 2rem;
+}
+
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 100ms ease-in-out;
+}
+</style>
