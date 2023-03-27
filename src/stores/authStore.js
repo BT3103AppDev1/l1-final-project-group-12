fe7874739a4b3a9a58e5a2c24d09f8aa66c46f9e
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-27 21:05:34 +0800

    added documentation to auth store

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -4,6 +4,16 @@ import { ref } from "vue";
 import { auth } from "../lib/firebase-config";
 import { getUserById } from "../lib/handlers/user";
 
+/*
+How to use auth system:
+1. Import stuff
+  - `import {storeToRefs} from "pinia";`
+  - `import { useAuthStore } from "@/stores/authStore.js";`
+2. Use store in your components
+  - `{ user } = storeToRefs(useAuthStore());`
+  - The `user` variable is a ref object, with properties as defined in the User object in "src/lib/models.js"
+*/
+
 const useAuthStore = defineStore("auth-store", () => {
   const user = ref();
 
@@ -17,4 +27,4 @@ const useAuthStore = defineStore("auth-store", () => {
   return { user };
 });
 
-export default useAuthStore;
+export { useAuthStore };
