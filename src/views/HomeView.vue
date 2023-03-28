b55ef79362d3d8da51eafcbf2a691400652b3cce
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-28 17:27:11 +0800

    update homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,54 @@
+<script setup>
+import { getAllListings } from "@/lib/handlers/listing";
+import { ref } from "vue";
+
+const studentListings = ref([]);
+const tutorListings = ref([]);
+
+const getListings = async () => {
+  studentListings.value = await getAllListings("student-listing");
+  tutorListings.value = await getAllListings("tutor-listing");
+};
+
+getListings();
+</script>
+
 <template>
-  <div id="background-image">
-    <div id="container">
-      <h1>Tent</h1>
-      <div id="button-array">
-        <button>Sign In</button>
-        <button>Sign Up</button>
+  <div id="listings-container">
+    <section id="student-listings-contianer">
+      <h1>Student Listings</h1>
+      <div class="student-listing-cards" v-for="studentListing in studentListings">
+        {{ studentListing }}
       </div>
-    </div>
+    </section>
+
+    <section id="tutor-listings-contianer">
+      <h1>Tutor Listings</h1>
+      <div class="tutor-listing-cards" v-for="tutorListing in tutorListings">
+        {{ tutorListing }}
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
-#background-image {
-  width: 100%;
+#listings-container {
   height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
-    url("../assets/tuitionimage.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
   display: flex;
+  align-items: start;
   justify-content: center;
+  gap: 2rem;
+  margin-top: 2rem;
 }
 
-#container {
+section {
+  max-width: 30%;
   display: flex;
   flex-direction: column;
-  width: 50%;
-  align-items: center;
-  margin-top: 10%;
-  padding: 3rem;
-  color: white;
+  gap: 1rem;
 }
 
-#container h1 {
-  font-size: 10rem;
-}
-
-#button-array {
-  width: 70%;
-  margin-top: 7%;
-  display: flex;
-  justify-content: space-around;
-}
-
-#button-array button {
-  background: inherit;
-  background-color: rgb(100, 100, 100);
-  border: none;
-  padding: 1rem;
-  border-radius: 1rem;
-  color: white;
-  font-size: 2rem;
-  cursor: pointer;
-}
-
-#button-array button:hover {
-  background-color: rgb(50, 50, 50);
-  transition: 150ms;
+section h1 {
+  text-align: center;
 }
 </style>
