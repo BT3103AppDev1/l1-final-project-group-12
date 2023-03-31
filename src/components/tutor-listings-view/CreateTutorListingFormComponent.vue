bd972f03c1e52229faa7215731e052187e186c93
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-31 21:42:43 +0800

    use modal component in student and tutor listings page

---
+++ src/components/tutor-listings-view/CreateTutorListingFormComponent.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { createListing } from "@/lib/handlers/listing";
+import { validateInputs } from "@/lib/utils";
+import { useAuthStore } from "@/stores/authStore";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+
+const { user } = storeToRefs(useAuthStore());
+
+const createListingInputs = ref({
+  subject: undefined,
+  level: undefined,
+  region: undefined,
+  description: undefined,
+  rates: undefined,
+});
+
+const createListingOnClick = async () => {
+  try {
+    validateInputs(createListingInputs.value);
+
+    await createListing("tutor-listing", {
+      userId: user.value.id,
+      dateCreated: new Date().toLocaleString(),
+      ...createListingInputs.value,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
+
+<template>
+  <div id="modal-content">
+    <h1>Add Tutor Listing</h1>
+
+    <form @submit.prevent="createListingOnClick">
+      <div id="input-container">
+        <label>Subject</label>
+        <input type="text" v-model="createListingInputs.subject" required />
+
+        <label>Level</label>
+        <input type="text" v-model="createListingInputs.level" required />
+
+        <label>Region</label>
+        <input type="text" v-model="createListingInputs.region" required />
+
+        <label>Description</label>
+        <input type="text" v-model="createListingInputs.description" required />
+      </div>
+
+      <button type="submit">Add Listing</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+#modal-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+}
+
+#input-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto auto;
+  column-gap: 1rem;
+  row-gap: 1rem;
+  align-items: center;
+}
+
+form input,
+form button {
+  padding: 0.2rem;
+  font-size: 1rem;
+}
+</style>
--- src/views/StudentListingsView.vue
+++ src/views/StudentListingsView.vue
@@ -1,27 +1,27 @@
 <script setup>
 import ModalComponent from "@/components/global-components/ModalComponent.vue";
-import CreateTutorListingFormComponent from "@/components/tutor-listings-view/CreateTutorListingFormComponent.vue";
+import CreateStudentListingFormComponent from "@/components/student-listings-view/CreateStudentListingFormComponent.vue";
 import { getAllListings } from "@/lib/handlers/listing";
 import { ref } from "vue";
 
 const showModal = ref(false);
 
-const allTutorListings = ref([]);
+const allStudentListings = ref([]);
 
-const getAllTutorListings = async () => {
-  allTutorListings.value = await getAllListings("tutor-listing");
+const getAllStudentListings = async () => {
+  allStudentListings.value = await getAllListings("student-listing");
 };
 
-getAllTutorListings();
+getAllStudentListings();
 </script>
 
 <template>
   <button @click="showModal = true">show modal</button>
 
-  <p>{{ allTutorListings }}</p>
+  <p>{{ allStudentListings }}</p>
 
   <ModalComponent v-show="showModal" @close-modal="showModal = false">
-    <CreateTutorListingFormComponent />
+    <CreateStudentListingFormComponent />
   </ModalComponent>
 </template>
 
