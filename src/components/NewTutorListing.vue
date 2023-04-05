948d38e0ddb6d26d193e2e7f7cc6110cbd6d1415
Xiaoboiboi <127714609+Xiaoboiboi@users.noreply.github.com>
2023-04-06 04:29:00 +0800

    made modals for Newstudentlisting and NewtutorListing close after clicking add lisitng button

--- src/components/NewTutorListing.vue
+++ src/components/NewTutorListing.vue
@@ -63,7 +63,7 @@
       </div>
 
       <div class="modal-footer">
-        <button type="button" class="button" @click="savetutorlisting">Add Listing</button>
+        <button type="button" class="button" @click="$emit('close-modal');savetutorlisting()">Add Listing</button>
       </div>
     </form>
 
