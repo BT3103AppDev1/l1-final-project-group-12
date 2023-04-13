f16bc952ca2597e1f51e63a6b6719af9deb36e96
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-13 13:31:32 +0800

    added padding to base layout

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -23,7 +23,9 @@ const logOutOnClick = async () => {
     </div>
   </nav>
 
-  <router-view />
+  <div id="container">
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
@@ -59,5 +61,12 @@ nav h1 {
 #navbar-buttons p:hover {
   text-decoration: underline white 2px;
   font-style: italic;
+}
+
+#container {
+  height: 100%;
+  margin-top: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
 }
 </style>
