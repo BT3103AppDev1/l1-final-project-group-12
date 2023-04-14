44163de53853b55992d8518da39b0e068a99d99a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-15 00:42:36 +0800

    add profile view

---
+++ src/components/my-profile-view/ListingCardComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ListingComponent from "@/components/global-components/ListingComponent.vue";
+import ModalComponent from "@/components/global-components/ModalComponent.vue";
+import { ref } from "vue";
+
+const { listing } = defineProps(["listing"]);
+const showModal = ref(false);
+</script>
+
+<template>
+  <ListingComponent @click="showModal = true">
+    <div>
+      <h3>{{ listing.subject }} ({{ listing.level }})</h3>
+      <p>{{ listing.region }}</p>
+    </div>
+
+    <h1>${{ listing.rates }}<span>/hr</span></h1>
+  </ListingComponent>
+
+  <ModalComponent v-show="showModal" @close-modal="showModal = false">
+    Edit listing here
+  </ModalComponent>
+</template>
+
+<style scoped>
+h1 {
+  color: var(--tent-orange);
+  font-size: 2rem;
+}
+
+h1 span {
+  font-size: 1.3rem;
+}
+</style>
