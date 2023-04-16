8f17d86da448eccf1f257748bef12f081c93413c
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-16 10:01:52 +0800

    add 404 page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <p>Oops! Nothing to see here...</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+h1 {
+  font-size: 8rem;
+}
+
+p {
+  font-size: 2rem;
+}
+</style>
